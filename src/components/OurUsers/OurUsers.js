e335e99187418f97fbb822071712d3b2c4929697
nizkiyVorobey <43185533+nizkiyVorobey@users.noreply.github.com>
2020-06-12 11:51:39 +0300

    Update OurUsers.js

--- src/components/OurUsers/OurUsers.js
+++ src/components/OurUsers/OurUsers.js
@@ -34,17 +34,6 @@ export const OurUsers = ({userList, setUserList, lastPage, setLastPage}) => {
             });
     }
 
-    useEffect(() => {
-        // console.log(userList);
-
-    }, [userList])
-
-    // useEffect(() => {
-    //     console.log(lastPage);
-
-    // }, [lastPage])
-
-
     return (
         <div className="cheerful-users">
             <p className="second-title">Our cheerful users</p>
@@ -60,77 +49,3 @@ export const OurUsers = ({userList, setUserList, lastPage, setLastPage}) => {
         </div>
     );
 };
-
-
-
-
-
-// export const OurUsers = () => {
-//     const [userList, setUserList] = useState([]);
-//     const [lastPage, setLastPage] = useState(false)
-
-//     useEffect(() => {
-//         getUsers()
-//     }, [])
-
-//     // get page and sort it by registration_timestamp
-//     const getUsers = () => {
-//         // count += 6;
-//         // page += 1;
-//         const getUserListCount = new Promise((resolve, reject) => {
-//             fetch(`https://frontend-test-assignment-api.abz.agency/api/v1/users?page=1&count=1`)
-//                 .then(function (response) {
-//                     return response.json();
-//                 })
-//                 .then(function (data) {
-//                     resolve(data)
-//                 })
-//                 .catch(function (error) {
-//                     console.log(error); // add alret with Error
-//                 });
-//         })
-
-//         getUserListCount
-//             .then((data) => {
-//                 fetch(`https://frontend-test-assignment-api.abz.agency/api/v1/users?page=1&count=${data.total_users}`)
-//                 .then((response) => {
-//                     return response.json();
-//                 })
-//                 .then(userList => {
-//                     userList.users.forEach(item => {
-//                         console.log(item.registration_timestamp);
-                        
-//                     })
-//                 })
-//             })
-//     }
-
-
-//     useEffect(() => {
-//         getUsers();
-//     }, [userList])
-
-//     useEffect(() => {
-//         console.log(userList);
-
-//     }, [userList])
-
-//     // useEffect(() => {
-//     //     console.log(lastPage);
-
-//     // }, [lastPage])
-
-
-//     return (
-//         <div className="cheerful-users">
-//             <p className="second-title">Our cheerful users</p>
-//             <p className="common-black-text cheerful-users-second-title">Attention! Sorting users by registration date</p>
-//             <UserList userList={userList} />
-//             {
-//                 !lastPage
-//                     ? <button className="button cheerful-users-btn">Show more</button>
-//                     : null
-//             }
-//         </div>
-//     );
-// };
